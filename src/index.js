7579305c88c388477fa0fbc27acd85cc407006cb
Chaitanya Varma <gchaitanya.369@gmail.com>
2023-06-11 03:00:43 +0530

    added vercel analytics

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ThemeProvider } from "@material-tailwind/react";
+import { inject } from "@vercel/analytics";
 
+inject();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
