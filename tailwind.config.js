a4c1eacf7e30ecf3af1165d8c8c07d5d9078049b
Chaitanya Varma <gchaitanya.369@gmail.com>
2023-06-23 16:32:24 +0530

    h-screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
   mode: "jit",
-  safelist: ['animate-[fade-in_1s_ease-in-out]', 'animate-[fade-in-down_1s_ease-in-out]', 'height-screen'],
+  safelist: ['animate-[fade-in_1s_ease-in-out]', 'animate-[fade-in-down_1s_ease-in-out]', 'h-screen'],
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {},
   plugins: [],
