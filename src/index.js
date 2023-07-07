89bd184d29f7136c657d865f84811b0f445006ba
Chaitanya Varma <gchaitanya.369@gmail.com>
2023-07-07 16:38:35 +0530

    removed unnecessary files

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ThemeProvider } from "@material-tailwind/react";
-import { inject } from "@vercel/analytics";
-
-inject();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
