24d45d87813dd41b2341a9b557566d106d18a362
Chaitanya Varma <gchaitanya.369@gmail.com>
2023-07-14 23:26:34 +0530

    brand new footer

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -10,72 +10,56 @@ import {
 
 const Footer = () => {
   const location = useLocation();
+  const [activeItem, setActiveItem] = useState("");
 
   const handleClick = () => {
     window.scrollTo(0, 0);
   };
 
+  useEffect(() => {
+    setActiveItem(location.pathname);
+  }, [location]);
+
   return (
     <footer
-      className="fixed bottom-0 bg-[#8d286f] bg-opacity-60 backdrop-blur-sm py-3 w-full footer-list"
+      className="bg-[#8d286f] bg-opacity-60 backdrop-blur-sm footer-list"
       style={{ zIndex: "200" }}
     >
-      <div className="flex space-x-4 justify-around items-center footer">
-        <Link
-          to="/"
-          className={location.pathname === "/" ? "active" : ""}
-          onClick={handleClick}
-        >
-          <FontAwesomeIcon
-            title="Home"
-            icon={faHouse}
-            size="2xl"
-            style={{ color: location.pathname === "/" ? "#319795" : "#ffffff" }}
-          />
-        </Link>
-        <Link
-          to="/about"
-          className={location.pathname === "/about" ? "active" : ""}
-          onClick={handleClick}
-        >
-          <FontAwesomeIcon
-            title="About"
-            icon={faUserTie}
-            size="2xl"
-            style={{
-              color: location.pathname === "/about" ? "#319795" : "#ffffff",
-            }}
-          />
-        </Link>
-        <Link
-          to="/projects"
-          className={location.pathname === "/projects" ? "active" : ""}
-          onClick={handleClick}
-        >
-          <FontAwesomeIcon
-            title="Projects"
-            icon={faLaptopCode}
-            size="2xl"
-            style={{
-              color: location.pathname === "/projects" ? "#319795" : "#ffffff",
-            }}
-          />
-        </Link>
-        <Link
-          to="/contact"
-          className={location.pathname === "/contact" ? "active" : ""}
-          onClick={handleClick}
-        >
-          <FontAwesomeIcon
-            title="Contact"
-            icon={faEnvelope}
-            size="2xl"
-            style={{
-              color: location.pathname === "/contact" ? "#319795" : "#ffffff",
-            }}
-          />
-        </Link>
-      </div>
+      <ul>
+        <li className={`list ${activeItem === "/" ? "active" : ""}`}>
+          <Link to="/" onClick={handleClick}>
+            <span className="icon">
+              <FontAwesomeIcon title="Home" icon={faHouse} size="xl" />
+            </span>
+            <span className="text">Home</span>
+          </Link>
+        </li>
+        <li className={`list ${activeItem === "/about" ? "active" : ""}`}>
+          <Link to="/about" onClick={handleClick}>
+            <span className="icon">
+              <FontAwesomeIcon title="About" icon={faUserTie} size="xl" />
+            </span>
+            <span className="text">About</span>
+          </Link>
+        </li>
+        <li className={`list ${activeItem === "/projects" ? "active" : ""}`}>
+          <Link to="/projects" onClick={handleClick}>
+            <span className="icon">
+              <FontAwesomeIcon title="Projects" icon={faLaptopCode} size="xl" />
+            </span>
+            <span className="text">Projects</span>
+          </Link>
+        </li>
+        <li className={`list ${activeItem === "/contact" ? "active" : ""}`}>
+          <Link to="/contact" onClick={handleClick}>
+            <span className="icon">
+              <FontAwesomeIcon title="Contact" icon={faEnvelope} size="xl" />
+            </span>
+            <span className="text">Contact</span>
+          </Link>
+        </li>
+        <div className="indicator"></div>
+      </ul>
     </footer>
   );
 };
