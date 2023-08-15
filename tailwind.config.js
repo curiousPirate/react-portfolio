37923af27bc4e563ca480402c207157ca930c947
Chaitanya Varma <gchaitanya.369@gmail.com>
2023-08-15 23:57:20 +0530

    minor UI updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,13 @@ module.exports = withMT({
     "./src/**/*.{js,jsx,ts,tsx}",
     "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {},
+  theme: {
+    colors: {
+      "th-primary": "var(--primary)",
+      "th-secondary": "var(--secondary)",
+      "th-basic": "var(--basic)",
+      "th-common": "var(--common)",
+    },
+  },
   plugins: [require("flowbite/plugin")],
 });
